29f3fa82c73c6d452bba1423e65e847328a90200
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-10-19 22:16:43 -0300

    req 4

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -2,16 +2,15 @@ import bcrypt from 'bcryptjs';
 import UserModel from '../database/models/user.model';
 import jwtUtil from '../middlewares/jwt.util';
 
-async function valid(foundUser: { dataValues: { password: string; }; }, password: string)
-  : Promise<object> {
-  if (!foundUser || foundUser.dataValues.password !== password) {
-    return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
-  }
-
+async function valid(
+  foundUser: { dataValues: { password: string; }; }, 
+  password: string,
+): Promise<boolean> {
   const isPasswordValid = await bcrypt.compare(password, foundUser.dataValues.password);
   if (!isPasswordValid) {
-    return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
+    return true;
   }
+  return false;
 }
 
 async function postLogin(body: { username: string; password: string; })
@@ -20,21 +19,21 @@ async function postLogin(body: { username: string; password: string; })
   if (!username || !password) {
     return { status: 'INVALID_KEY', data: { message: '"username" and "password" are required' } };
   }
+
   const foundUser = await UserModel.findOne({ where: { username } });
-  const vali = valid(foundUser, password);
-  // if (!foundUser || foundUser.dataValues.password !== password) {
-  //   return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
-  // }
+  if (!foundUser) {
+    return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
+  }
 
-  // const isPasswordValid = await bcrypt.compare(password, foundUser.dataValues.password);
-  // if (!isPasswordValid) {
-  //   return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
-  // }
-
+  const vali = await valid(foundUser, password);
+  if (vali) {
+    return { status: 'UNAUTHORIZED', data: { message: 'Username or password invalid' } };
+  }
+  
   const { id } = foundUser.dataValues;
   const token = jwtUtil.sign({ id, username: foundUser.dataValues.username });
 
-  return { status: 'CREATED', data: { token } };
+  return { status: 'SUCCESSFUL', data: { token } };
 }
 
 export default {
--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -1,8 +1,9 @@
-import chai, { expect } from 'chai';
 import sinon from 'sinon';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
-
+import app from '../../../src/app';
 chai.use(sinonChai);
 
 describe('LoginController', function () {
@@ -13,6 +14,19 @@ describe('LoginController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  it('post /products', async function () {
+    const httpRequestBody = {
+      "username": "Hagar",
+      "password": "asd"
+    };
+    const httpResponse = await chai.request(app).post('/login').send(httpRequestBody);
+
+    expect(httpResponse.status).to.equal(401);
+    expect(httpResponse.body).to.be.deep.equal({
+      message: 'Username or password invalid'
+    });
   });
 
 });
